13d16cb0893f96329b8e413adf08aa96f63acb3c
simplify20 <1160934729@qq.com>
2016-04-10 08:08:36 +0800

    add holder-annotation lib

--- holder-annotations/src/main/java/com/steve/creact/annotation/DataBean.java
+++ holder-annotations/src/main/java/com/steve/creact/annotation/DataBean.java
@@ -1,4 +1,4 @@
-package com.steve.creact.library.annotation;
+package com.steve.creact.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,17 +10,13 @@ import java.lang.annotation.Target;
  * @date:2016/3/31
  * @email:yangjianjun@117go.com
  */
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 public @interface DataBean {
 
     String beanName();
 
-    String packageName();
+    int layout() default 0;
 
-    Class data();
-
-    Class holder();
-
-    int layout();
+    Class<?> data();
 }

422cfad937732b1d0cce4dc65172c0a36fd4e349
simplify20 <1160934729@qq.com>
2016-04-10 08:09:36 +0800

    update holder-complier

---
+++ holder-complier/src/main/java/com/steve/creact/processor/model/BeanInfo.java
@@ -0,0 +1,15 @@
+package com.steve.creact.processor.model;
+
+/**
+ * Created by Administrator on 2016/4/9.
+ */
+public class BeanInfo {
+
+    public String holderName;
+    public String holderPackage;
+    public String dataName;
+    public String dataPackage;
+    public String dataBeanName;
+    public String dataBeanPackage;
+    public int layoutId;
+}
